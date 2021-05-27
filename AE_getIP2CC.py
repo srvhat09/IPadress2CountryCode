c8f688ea489811da7815e632d957741b289e63ab
srvhat09 <srvhat09@anaheim-eng.co.jp>
2021-05-27 15:16:07 +0900

    first commit

---
+++ AE_getIP2CC.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import subprocess
+
+TAG = __name__ + ' {}:'
+
+
+def parser():
+    """
+    依頼パラメタをパースする
+    """
+
+    argDict = {}
+    usage = """
+Usage:
+    AE_getIP2CC.py [-i | --ip <ip address string> ]
+    AE_getIP2CC.py -h | --help
+
+Options:
+    IPアドレスから割当Country Code取得機能
+    -i --ip <ip address string> ip address
+    -h --help                   当ヘルプを表示
+
+    ex) AE_getIP2CC.py -i xx.xx.xx.xx
+        → DE
+""".format(__file__)
+    arguments = sys.argv
+    if len(arguments) == 1:
+        # 引数が無いため終了
+            print(usage)
+            sys.exit()              #強制終了
+    # ファイル自身を指す最初の引数を除去
+    arguments.pop(0)
+    # 引数として与えられたoption check
+    if len(arguments) >= 2:
+        opt = arguments[1]
+        if opt.startswith('-'):     #2個目は必ず[-]では始まらない
+            print(usage)
+            sys.exit()              #強制終了
+
+    # - で始まるoption
+    options = [option for option in arguments if option.startswith('-')]
+
+    if '-h' in options or '--help' in options:
+        print(usage)
+        sys.exit()              #強制終了
+
+    if '-i' in options or '--ip' in options:
+        type_position = arguments.index('-i') \
+            if '-i' in options else arguments.index('--ip')
+        argDict["ip"] = arguments[type_position + 1]
+
+    return argDict
+
+
+# 入力パラメタチェック: argチェック
+class ParamCheck():
+    def chk_param(self, dic: dict):
+        num = len(dic)
+        if num == 0:
+            return False
+
+        if 'ip' in dic:
+            return True
+
+        return False
+
+
+# メイン処理
+def main():
+    cmd = 'nslookup -type=txt {}.cc.wariate.jp | grep text'
+    try:
+        argDict = parser()
+        mc = ParamCheck()
+        if not mc.chk_param(argDict):
+            raise ValueError(TAG.format('main') + 'param error')
+            sys.exit()              #強制終了
+
+        ipList = argDict['ip'].split(".")
+        reverseIP = ipList[3] + "." + ipList[2] + "." + ipList[1] + "." + ipList[0]
+        cmd = cmd.format(reverseIP)
+        process = (subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                           shell=True).communicate()[0]).decode('utf-8')
+        processList = process.split('"')
+        print(processList[1])
+
+    except Exception as e:
+        print('Error:' + e.args[0])
+
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # IPadress2CountryCode
+
+IPアドレスから割当国名を取得する方法としては、ARIN,APNIC,LACNIC,RIPE,AFRINICの公開しているIPアドレスの割り当て一覧表から検索する方法しか知りませんでした。
+
+ある時、DNSサービスとしてこれを運営して下さっている内容を見つけ、これを呼び出すだけの簡単なPyhtonスクリプトを作成しました。
+> http://cc.wariate.jp/
+
+※定期的に更新されているようで、感謝しております。
+
+> python3 AE_getIP2CC.py -h
+>
+> Usage:
+>     AE_getIP2CC.py [-i | --ip <ip address string> ]
+>     AE_getIP2CC.py -h | --help
+> 
+> Options:
+>     IPアドレスから割当Country Code取得機能
+>     -i --ip <ip address string> ip address
+>     -h --help                   当ヘルプを表示
+> 
+>     ex) AE_getIP2CC.py -i xx.xx.xx.xx
+>         → DE
+
+動作環境としては、nslookupとgrepがLinuxと同じ出力ができれば、Windowsでも可能だと思います。（未検証）
+私は、WSL1のUbuntu 18.04で動作検証済です。勿論、Ubuntu本体でも問題ないかと思います。
+
+Pythonのnslookup系ライブラリを試したのですが、TXTの扱いが無い等があり結局はOSコマンドを投げて対応しています。
+
+Python3.x系でしか動作しないかとは思いますが、2.x系は未検証です。
